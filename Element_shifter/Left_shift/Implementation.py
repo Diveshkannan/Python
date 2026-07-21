88e758404b8987c04c3f1ea5d9db8234c427a4c5
Divesh Kannan T <diveshkannan.tr@gmail.com>
2026-07-21 19:26:50 +0530

    Create Implementation.py

---
+++ Element_shifter/Left_shift/Implementation.py
@@ -0,0 +1,17 @@
+def Lshift(Arr,n):
+    length=len(Arr)
+    List=[None]*length
+    for i in range(length):
+          tmp=((i-n)%(length))
+          List[tmp]=Arr[i]
+    Arr[:]=List
+L=[10,20,30,40,12,11]
+n=int(input("Number:"))
+if (n<=0):
+    print("Invalid input")
+else:
+    Lshift(L,n)
+print(L)
+    
+     
+
